19efa290fae0f66f0b9ab71b583b6a722ac65263
kumarisupriya302 <kumarisupriya30200@gmail.com>
2025-03-20 10:37:26 +0530

    add about.php and terms & conditions page

---
+++ about.php
@@ -0,0 +1,95 @@
+<?php
+include_once('kheader.php');
+?>
+<!--body start-->
+<!--banner section start-->
+<div class="breadcumb-wrapper" data-bg-src="assets/img/bg/about-bg.webp">
+        <div class="container">
+            <div class="breadcumb-content">
+                <h1 class="breadcumb-title">About Us</h1>
+                <ul class="breadcumb-menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li>About Us</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+<!--banner section end-->
+<!--about section start-->
+<div class="about-area position-relative overflow-hidden overflow-hidden space" id="about-sec">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-7">
+                    <div class="img-box3">
+                        <div class="img1"><img src="assets/img/normal/image1.webp" alt="About"></div>
+                        <div class="img2"><img src="assets/img/normal/image2.webp" alt="About"></div>
+                        <div class="img3 movingX"><img src="assets/img/normal/image3.webp" alt="About"></div>
+                    </div>
+                </div>
+                <div class="col-xl-5">
+                    <div class="ps-xl-4">
+                        <div class="title-area mb-20"><span class="sub-title style1">Karma Experience</span>
+                            <h2 class="sec-title mb-20 pe-xl-5 me-xl-5 heading">Enjoy a Luxury Experience
+                            </h2>
+                        </div>
+                        <p class="pe-xl-5">Discover the world of luxury with Karma Experience, the marketing division of the award-winning Karma Group. We specialize in offering exclusive, heavily discounted promotional holidays, giving you the chance to explore the globe while experiencing the exceptional hospitality and luxurious comfort that define the Karma brand.</p>
+                        <!-- //<p class="mb-30 pe-xl-5">Our carefully curated destinations include everything from sun-kissed beaches and vibrant cityscapes to peaceful mountain retreats. Whether you're seeking adventure, relaxation, or cultural exploration, Karma Experience has something special for every traveler.</p> -->
+                        <div class="about-item-wrap">
+                            <div class="about-item style2">
+                                <div class="about-item_img"><img src="assets/img/icon/about_1_1.svg" alt=""></div>
+                                <div class="about-item_centent">
+                                    <h5 class="box-title">Exclusive Holidays</h5>
+                                    <p class="about-item_text">Explore world-class destinations at unbeatable prices, tailored to give you a luxury experience like no other.</p>
+                                </div>
+                            </div>
+                            <div class="about-item style2">
+                                <div class="about-item_img"><img src="assets/img/icon/about_1_2.svg" alt=""></div>
+                                <div class="about-item_centent">
+                                    <h5 class="box-title">Exceptional Hospitality</h5>
+                                    <p class="about-item_text">Our resorts are designed to offer the perfect blend of comfort, elegance, and local charm.</p>
+                                </div>
+                            </div>
+                            <div class="about-item style2">
+                                <div class="about-item_img"><img src="assets/img/icon/about_1_3.svg" alt=""></div>
+                                <div class="about-item_centent">
+                                    <h5 class="box-title">Adventure & Relaxation</h5>
+                                    <p class="about-item_text">Whether you crave excitement or peace, you’ll find the perfect balance with Karma Experience.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="about-item_text">Don’t miss this opportunity to explore the Karma verse and experience the very best!</p>
+                        <!-- <div class="mt-35"><a href="contact.html" class="th-btn style3 th-icon">Contact With Us</a>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+            <div class="shape-mockup movingX d-none d-xxl-block" data-top="0%" data-left="-18%"><img
+                    src="assets/img/shape/shape_2_1.png" alt="shape"></div>
+            <div class="shape-mockup jump d-none d-xxl-block" data-top="28%" data-right="-15%"><img
+                    src="assets/img/shape/shape_2_2.png" alt="shape"></div>
+            <div class="shape-mockup spin d-none d-xxl-block" data-bottom="18%" data-left="-112%"><img
+                    src="assets/img/shape/shape_2_3.png" alt="shape"></div>
+            <div class="shape-mockup movixgX d-none d-xxl-block" data-bottom="18%" data-right="-12%"><img
+                    src="assets/img/shape/shape_2_4.png" alt="shape"></div>
+        </div>
+    </div>
+<!--about section end-->
+<!--video section start-->
+<!-- <div class="video-section">
+    <div class="container">
+        <div class="row">
+            <div class="video-img2">
+                <img src="assets/img/bg/video-img.png" alt="About"> 
+                <a href="https://www.youtube.com/watch?v=8qbaWTW0Lqo" class="play-btn popup-video">
+                    <i class="fa-sharp fa-solid fa-play"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div> -->
+<!--video section end-->
+<!--body end-->
+
+<?php
+include_once('kfooter.php');
+?>
---
+++ terms-and-conditions.php
@@ -0,0 +1,91 @@
+<?php
+include_once('kheader.php');
+?>
+
+<!--body start-->
+
+<!--banner section start-->
+
+<div class="breadcumb-wrapper over-lay" data-bg-src="assets/img/bg/terms-conditions-banner.webp">
+    <div class="t-overlay"></div>
+        <div class="container">
+            <div class="breadcumb-content">
+                <h1 class="breadcumb-title">Terms & Conditions</h1>
+                <ul class="breadcumb-menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li>Terms & Conditions</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+<!--banner section end-->
+
+<div class="container">
+<div class="conditions">
+    <div class="condition-data">
+        <h5>Website Terms of Use</h5>
+        <p>This Karma Group website (“Site”) is made available to you, the user, member or customer subject to the terms and conditions set out below (“Terms”)</p>
+    </div>
+    <div class="conditions-term">
+        <ul>
+            <li>
+                <h5>1. WHO ARE WE?</h5>
+                <p>The Site is operated by Karma Resorts Pte Ltd (Company Number 200509501G) a company registered in Singapore with its registered office being160 Robinson Road, #18-07 Singapore 068914. Karma Resorts Pte Ltd is part of the Karma Group and the Terms apply to all other related companies and businesses which form part of the Karma Group. References to “we” “us” and “our” in these Terms refer to the above entities.</p>
+            </li>
+            <li>
+                <h5>2. CONTENT ON THIS SITE</h5>
+                <p>Your use of this Site and its contents including, without limit, any text, data, logos, graphics, photographs, images, animations, software, apps, forms, videos, music and other audio/visual materials that you access (“Content”) is subject to these Terms. Any Content that you access on this Site is either owned by us (or third parties who license such Content to us) and is made available only for your own personal use on the condition that you must not republish, post, transmit, edit, adapt, syndicate or distribute any Content without our prior written permission. The names ‘Karma Group’, ‘Karma Resorts’, ‘Karma Retreats’, ‘Karma Royal’, ‘Karma Estates’, ‘Karma Spa’, or ‘Karma Beach’ and any other business names and logos displayed on this Site may be trade marks belonging to us or other companies in Karma Group (“Trade Marks”).</p>
+                <p>To the maximum extent legally permitted, you cannot link to or seek to extract data from this Site or reutilise any part of this Site or Content, for any commercial purpose or use our Trade Marks in a way that suggests that you or your business has any endorsement from or affiliation with the Karma Group without our prior written permission (at our sole discretion).</p>
+            </li>
+            <li>
+                <h5>3. LINKS TO OTHER WEBSITES</h5>
+                <p>This Site may link to third party websites from time to time. These links are provided for your convenience only. We do not control third party websites and are not responsible for their content or how they operate. Where this Site includes any links to third party websites, this does not imply any endorsement by us of the goods, services or materials made available on such websites. You acknowledge that (to the maximum extent legally permitted and unless we state otherwise) we shall not be liable in respect of your use of those third party websites or any purchase you make through them.</p>
+            </li>
+            <li>
+                <h5>4. YOUR USE OF THIS SITE</h5>
+                <p>You must only use this Site for legal purposes in accordance with these Terms and are prohibited from using our Site to engage in any fraudulent activity or in a manner that (in our reasonable opinion) is liable to damage our business or harm other users. Your use of our Site is subject to our membership rules and other policies or guidelines that we may communicate to you from time to time.</p>
+                <p>You also undertake that any personal data and other information you may provide to us when registering or signing up to any services on this Site is complete, accurate and up to date. In relation to any material you submit to us or post on this Site, you undertake to us that either you own this material or have the necessary rights, clearances and or approvals you need to submit or post such material.</p>
+                <p>You agree to compensate us in relation to any third party legal actions or claims that are made against us and for any associated losses, damages or expenses (including any legal expenses) that we suffer as a result of you breaching your obligations or undertakings in this Section 4.</p>
+            </li>
+            <li>
+                <h5>5. OUR LIABILITY</h5>
+                <p>Nothing in these Terms shall exclude or limit our liability for fraud or for death or personal injury resulting from our negligence (or the negligence of our employees or agents) or where applicable law does not permit us to restrict or exclude particular obligations or liabilities.</p>
+                <p>Subject the above paragraph we do not accept any liability for damage to any computer or device that you use to access this Site or in relation to any loss of data when you use this Site. In addition, we cannot guarantee that any Content you access or download is free from viruses, worms or other malware, and you should check that you have suitable, up to date virus or malware protection on your computer or device.</p>
+                <p>While we will correct any errors on this Site that come to our attention as soon as we reasonably can, we do not undertake or warrant that this Site or the Content will be completely free from bugs or errors or that this Site will be available on an uninterrupted basis. You accept that access to this Site (or certain features) may be interrupted or suspended without notice in the case of IT system issues or where we need to undertake maintenance or due to other reasons beyond our reasonable control.</p>
+                <p>We do not make any representation and exclude all warranties, terms or conditions (whether express or implied by law or otherwise) in respect of the Site or its Content, including, without limitation, any decision you take on the basis of information provided through this Site, except that nothing in these Terms shall exclude or restrict your statutory rights or other rights that cannot be excluded or restricted under applicable law. Except as set out above, we will not be liable for (1) any indirect, consequential or special loss or damage; or (2) any lost revenue, profits or other economic loss (whether direct or indirect), however arising from your use of this Site.</p>
+            </li>
+            <li>
+                <h5>6. PRIVACY & SECURITY</h5>
+                <p>We take the collection, usage and security of your personal data seriously.</p>
+                <p>Karma receives and processes certain types of personal data about its customers. This allows us to provide our services to our customers.</p>
+                <li>We use information about our customers or those connected to our customers for a number of reasons. We encourage you to take a look at our Privacy Notice so you have all the information you need. Any questions, we’re always happy to talk to you more about this.</li>
+            </li>
+            <li>
+                <h5>7. CHANGES TO THESE TERMS</h5>
+                <p>We may amend these Terms at any time, for example, where we add new services or features or where there is a change in applicable law. All such changes will take effect once these Terms have been updated and you will be assumed to have accepted any such changes if you continue to use the Site after the latest update.</p>
+            </li>
+            <li>
+                <h5>8. PURCHASE OF PRODUCTS OR SERVICES</h5>
+                <p>If you elect to purchase any products or services which we offer to you through this Site, (or any affiliated website of the Karma Group) this may be subject to new and/or additional terms and conditions which will be notified to you at the time.</p>
+            </li>
+            <li>
+                <h5>9. INVAILIDITY & WAIVER</h5>
+                <p>If any of these Terms are found by a court or a regulator, to be invalid or unenforceable the other provisions shall continue to apply to the maximum extent legally permitted. No waiver of any of these Terms shall be effective unless made in writing by us and a waiver (or non-enforcement) shall not be construed as a waiver of any subsequent breach.</p>
+            </li>
+            <li>
+                <h5>10. GOVERNING LAW</h5>
+                <p>In the event of any dispute between you and us concerning these Terms, the laws of Singapore will apply. If you wish to take court proceedings against us, you should do so within Singapore, except to the extent that applicable law in your country of residence requires mandatory application of another law and/or jurisdiction and/or language, and such requirement cannot lawfully be excluded under these Terms, in which case such law and/or jurisdiction and/or language shall apply, as far as legally required.</p>
+            </li>
+            <li>
+                <h5>11. CONTACT</h5>
+                <p>If you have any questions or complaints regarding the Site or these Terms, you can contact us by emailing info@karmagroup.com.</p>
+            </li>
+        </ul>
+    </div>
+</div>
+</div>
+<!--body end-->
+
+<?php
+include_once('kfooter.php');
+?>
